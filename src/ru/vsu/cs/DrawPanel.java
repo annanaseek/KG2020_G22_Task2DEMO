07de75e7f787fb9f7e6984c25c711e4cad534159
Annaseek <agapova.nyura@bk.ru>
2020-09-09 16:02:46 +0300

    Рисование снежинки независимо от Graphics

--- src/ru/vsu/cs/DrawPanel.java
+++ src/ru/vsu/cs/DrawPanel.java
@@ -6,16 +6,17 @@ import java.awt.*;
 public class DrawPanel extends JPanel {
     @Override
     public void paint(Graphics g) {
-        drawSnowFlake(g, 400, 300, 150, 28);
+        LineDrawer ld = new GraphicsLineDrawer(g);
+        drawSnowFlake(ld, 400, 300, 150, 28);
     }
 
-    public static void drawSnowFlake(Graphics g, int x, int y, int r, int n) {
+    public static void drawSnowFlake(LineDrawer ld, int x, int y, int r, int n) {
         double da = 2 * Math.PI / n;
         for (int i = 0; i < n; i++) {
             double a = da * i;
             double dx = r * Math.cos(a);
             double dy = r * Math.sin(a);
-            g.drawLine(x, y, x + (int) dx, y + (int) dy);
+            ld.drawLine(x, y, x + (int) dx, y + (int) dy);
         }
     }
 }
