2918ef3b7446219126e6e0f474d55547f562e384
Annaseek <agapova.nyura@bk.ru>
2020-09-09 16:18:14 +0300

    Добавление Buffered Image

--- src/ru/vsu/cs/DrawPanel.java
+++ src/ru/vsu/cs/DrawPanel.java
@@ -2,12 +2,23 @@ package ru.vsu.cs;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
 
 public class DrawPanel extends JPanel {
     @Override
     public void paint(Graphics g) {
-        LineDrawer ld = new GraphicsLineDrawer(g);
+        BufferedImage bi = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
+        Graphics gr = bi.createGraphics();
+
+        gr.setColor(Color.WHITE);
+        gr.fillRect(0, 0, getWidth(), getHeight());
+        gr.setColor(new Color(99, 204, 255));
+        LineDrawer ld = new GraphicsLineDrawer(gr);
         drawSnowFlake(ld, 400, 300, 150, 28);
+
+        g.drawImage(bi, 0, 0, null);
+        gr.dispose();
     }
 
     public static void drawSnowFlake(LineDrawer ld, int x, int y, int r, int n) {
